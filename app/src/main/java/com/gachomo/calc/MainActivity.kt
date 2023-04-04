32612616382a1c486064ef7f283698ecade31f9f
ezragach21 <gachomoezra@gmail.com>
2023-04-05 01:13:30 +0300

    Initial commit

---
+++ app/src/main/java/com/gachomo/calc/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.gachomo.calc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getColor
+import kotlinx.android.synthetic.main.activity_main.*
+import org.mariuszgromada.math.mxparser.Expression
+import java.text.DecimalFormat
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button_clear.setOnClickListener {
+            input.text = ""
+            output.text = ""
+        }
+
+        button_bracket_left.setOnClickListener {
+            input.text = addToInputText("(")
+        }
+        button_bracket_right.setOnClickListener{
+            input.text = addToInputText(")")
+        }
+        button_0.setOnClickListener {
+            input.text = addToInputText("0")
+        }
+        button_1.setOnClickListener {
+            input.text = addToInputText("1")
+        }
+        button_2.setOnClickListener {
+            input.text = addToInputText("2")
+        }
+        button_3.setOnClickListener {
+            input.text = addToInputText("3")
+        }
+        button_4.setOnClickListener {
+            input.text = addToInputText("4")
+        }
+        button_5.setOnClickListener {
+            input.text = addToInputText("5")
+        }
+        button_6.setOnClickListener {
+            input.text = addToInputText("6")
+        }
+        button_7.setOnClickListener {
+            input.text = addToInputText("7")
+        }
+        button_8.setOnClickListener {
+            input.text = addToInputText("8")
+        }
+        button_9.setOnClickListener {
+            input.text = addToInputText("9")
+        }
+        button_division.setOnClickListener {
+            input.text = addToInputText("÷")
+        }
+        button_multiply.setOnClickListener {
+            input.text = addToInputText("×")
+        }
+        button_subtraction.setOnClickListener {
+            input.text = addToInputText("-")
+        }
+        button_addition.setOnClickListener {
+            input.text = addToInputText("+")
+        }
+        button_dot.setOnClickListener {
+            input.text = addToInputText(".")
+        }
+        button_equals.setOnClickListener {
+            showResult()
+        }
+    }
+
+    private fun addToInputText(buttonValue: String): String{
+        return "${input.text}$buttonValue"
+    }
+
+    private fun getInputExpression(): String{
+        var expression = input.text.replace(Regex("÷"),("/"))
+        expression = expression.replace(Regex("×"),("*"))
+        return expression
+    }
+
+    private fun showResult() = try{
+        val expression = getInputExpression()
+        val result = Expression(expression).calculate()
+        if(result.isNaN()){
+            output.text = "Error!!"
+            output.setTextColor(getColor(this, R.color.red))
+        }else{
+            // Show result
+            output.text = DecimalFormat("0.#########").format(result).toString()
+            output.setTextColor(getColor(this, R.color.green))
+        }
+    }catch(e: Exception){
+        // Result
+        output.text = "Error!!"
+        output.setTextColor(getColor(this, R.color.red))
+    }
+}
